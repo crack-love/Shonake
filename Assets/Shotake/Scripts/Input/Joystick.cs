4d5a3e71c58bba453cea9815e5c0982501194c80
KYG_Ideapad <masterchip@naver.com>
2022-08-10 21:35:33 +0900

    Smooth Joystick threshold

---
+++ Assets/Shotake/Scripts/Input/Joystick.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Shotake
+{
+    sealed class Joystick : UIObject, IPointerDownHandler, IDragHandler, IPointerUpHandler
+    {
+        [SerializeField] float m_range = 1;
+        [SerializeField] float m_threshold = 0.1f;
+        [SerializeField] RectTransform m_borderImage;
+        [SerializeField] RectTransform m_fillImage;        
+
+        Canvas m_canvas;
+        public Vector2 m_axis;
+        Vector2 m_begPos;        
+        
+        new void Awake()
+        {
+            base.Awake();
+            m_canvas = GetComponentInParent<Canvas>();
+        }
+
+        public Vector2 GetAxis()
+        {
+            return m_axis;
+        }
+
+        void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+        {
+            m_borderImage.gameObject.SetActive(true);
+            m_fillImage.gameObject.SetActive(true);
+            m_borderImage.position = eventData.position;
+            m_fillImage.position = eventData.position;
+            m_begPos = eventData.position;
+        }
+
+        void IDragHandler.OnDrag(PointerEventData eventData)
+        {
+            float width = m_borderImage.rect.width * m_canvas.scaleFactor;
+            Vector2 offset = eventData.position - m_begPos;
+            Vector2 offsetC = Vector2.ClampMagnitude(offset, width * m_range);
+            
+            if (offsetC.sqrMagnitude < m_threshold * width * m_threshold * width)
+            {
+                m_fillImage.position = m_begPos;
+                m_axis = Vector2.zero;
+                return;
+            }
+
+            Vector2 offsetCT = Vector2.ClampMagnitude(offsetC, width * m_threshold);
+            m_axis = (offsetC - offsetCT) / (width * m_range - m_threshold * width);
+            m_fillImage.position = m_begPos + offsetC;
+        }
+
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+        {
+            m_borderImage.gameObject.SetActive(false);
+            m_fillImage.gameObject.SetActive(false);
+            m_axis = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Shotake/Scripts/Input/UIObjectManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityCommon;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Shotake
+{
+    class UIObjectManager : MonobehaviourSingletone<UIObjectManager>
+    {
+        List<UIObject> m_objects = new List<UIObject>();
+        
+        public UIObject GetObject(string name)
+        {
+            foreach (UIObject o in m_objects)
+            {
+                if (name == o.UIName)
+                {
+                    return o;
+                }
+            }
+            return null;
+        }
+
+        public UIObject GetObject(int idx)
+        {
+            return m_objects[idx];
+        }
+
+        public int AddObject(UIObject o)
+        {
+            m_objects.Add(o);
+            return m_objects.Count - 1;
+        }
+
+        public bool RemoveObject(UIObject o)
+        {
+            return m_objects.Remove(o);
+        }
+
+        public void RemoveObject(int idx)
+        {
+            m_objects.RemoveAt(idx);
+        }
+
+#if UNITY_EDITOR
+        [CustomEditor(typeof(UIObjectManager))]
+        class UIObjectManagerEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                var target = (UIObjectManager)this.target;
+
+                if (target.m_objects.Count > 0)
+                {
+                    GUILayout.BeginVertical();
+                    GUILayout.Label("Registed Objects");
+                    foreach (UIObject o in target.m_objects)
+                    {
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Index : " + o.UIIndex, GUILayout.Width(100));
+                        if (GUILayout.Button(o.UIName, GUILayout.ExpandWidth(true)))
+                        {
+                            EditorGUIUtility.PingObject(o);
+                        }
+                        GUILayout.EndHorizontal();
+                    }
+                    GUILayout.EndVertical();
+                }
+            }
+        }
+#endif
+    }
+}
